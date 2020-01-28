98f16ad807046125dd0b554035b53cdbb31da51f
Andyloz <lozandyloz@gmail.com>
2020-01-28 17:46:05 +0100

    Ejercicio 1 hecho

---
+++ src/main/java/daw/actividades/relaciond/Ejercicio1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties,
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor,
+ */
+package daw.actividades.relaciond;
+
+/**
+ *
+ * @author lozan
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int array[] = {1,2,3,4,5,6};
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
