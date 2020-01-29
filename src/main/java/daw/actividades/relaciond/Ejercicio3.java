60392510221d7ed960acdba29ab569fa7dc22ab0
Andyloz <lozandyloz@gmail.com>
2020-01-29 13:44:28 +0100

    Ejercicio 3 hecho. Faltan los comentarios

--- src/main/java/daw/actividades/relaciond/Ejercicio3.java
+++ src/main/java/daw/actividades/relaciond/Ejercicio3.java
@@ -5,10 +5,37 @@
  */
 package daw.actividades.relaciond;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author lozan
  */
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+        double array[] = new double[20];
+        
+        // Rellena con doubles de 7.5
+        Arrays.fill(array, 7.5);
+        
+        // Copia de array
+        double array2[] = array.clone();
+        
+        // Comparar ambos arrays
+        System.out.println(array.equals(array2));
+        
+        Random random = new Random();
+        array[random.nextInt(array.length)] = 6.3;
+        
+        for (double d : array) {
+            System.out.println(d);
+        }
+        
+        for (double d : array2) {
+            System.out.println(d);
+        }
+        
+        System.out.println(array.equals(array2));
+    }
 }
