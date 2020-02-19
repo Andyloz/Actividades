59a824b14cbb310042dc3b626ff958b9dbf8ea80
Andyloz <lozandyloz@gmail.com>
2020-02-19 14:46:05 +0100

    Comienzo de listaCancion

---
+++ src/main/java/daw/actividades/relaciond/E24/Cancion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.actividades.relaciond.E24;
+
+/**
+ *
+ * @author andyloz
+ */
+public class Cancion {
+    private final String nombre;
+    private final String album;
+    private final String duracionCancion;
+
+    public Cancion(String nombre, String album, String duracionCancion) {
+        this.nombre = nombre;
+        this.album = album;
+        this.duracionCancion = duracionCancion;
+    }
+}
---
+++ src/main/java/daw/actividades/relaciond/E24/ListaReproduccion.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.actividades.relaciond.E24;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author andyloz
+ */
+public class ListaReproduccion {
+    private ArrayList<Cancion> lista;
+
+    public ListaReproduccion(ArrayList<Cancion> lista) {
+        this.lista = lista;
+    }
+
+    public ArrayList<Cancion> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Cancion> lista) {
+        this.lista = lista;
+    }
+    
+    public int numeroCanciones() {
+        return lista.size();
+    }
+    
+    public boolean estaVacia() {
+        return lista.isEmpty();
+    }
+    
+    public Cancion escucharCancion(int index) {
+        return lista.get(index);
+    }
+    
+    public void cambiarCancion(int index, Cancion cancion) {
+        lista.set(index, cancion);
+    }
+    
+    public void grabarCancion(Cancion cancion) {
+        lista.add(cancion);
+    }
+    
+    public void eliminarCancion(int index) {
+        lista.remove(index);
+    }
+    
+    public String imprimirLista() {
+        
+    }
+}
